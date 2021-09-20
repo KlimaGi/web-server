3a108b8487793aa3addccb3a08844b8a61f5e95a
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-20 20:57:08 +0300

    serving up static assets

--- src/app.js
+++ src/app.js
@@ -1,26 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("<h2>Weather</h2>");
-});
-
-app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "Giedre",
-      age: 31,
-    },
-    {
-      name: "Vy",
-    },
-  ]);
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h2>About page</h2>");
-});
+const publicDirPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirPath));
 
 app.get("/weather", (req, res) => {
   res.send({
