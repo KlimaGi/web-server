bc3338973cf2df96cacd2b00ed122dae1a95b298
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-21 23:51:02 +0300

    add styles

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,14 @@ console.log("Client side javascript is loaded!");
 
 const weatherForm = document.querySelector("form");
 const searchEl = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
 
   const location = searchEl.value;
   const url = `http://localhost:3000/weather?address=${location}`;
@@ -12,10 +17,10 @@ weatherForm.addEventListener("submit", (e) => {
   fetch(url).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        console.log(data.error);
+        messageOne.textContent = data.error;
       } else {
-        console.log(data.location);
-        console.log(data.forecast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
       }
     });
   });
