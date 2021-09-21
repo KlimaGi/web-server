ab4e5b2a59a742a7475ce155221cf94cc53ab2ca
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-21 13:47:08 +0300

    dynamic pages with templating, hbs

--- src/app.js
+++ src/app.js
@@ -2,9 +2,30 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-
 const publicDirPath = path.join(__dirname, "../public");
+
+app.set("view engine", "hbs");
 app.use(express.static(publicDirPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Gi",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Gi",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
