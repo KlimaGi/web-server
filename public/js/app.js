678e3c27b4f5de070a10339f4678b15d92dd0110
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-21 21:56:09 +0300

    creating a searching form

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,22 @@
 console.log("Client side javascript is loaded!");
 
-const url = "http://localhost:3000/weather?address=boston";
-fetch(url).then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
+const weatherForm = document.querySelector("form");
+const searchEl = document.querySelector("input");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = searchEl.value;
+  const url = `http://localhost:3000/weather?address=${location}`;
+
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
   });
 });
