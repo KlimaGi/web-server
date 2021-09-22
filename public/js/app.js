69682cc5a2c1a61f71d7db0ad053b56a99ca160b
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-22 15:22:33 +0300

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javascript is loaded!");
-
 const weatherForm = document.querySelector("form");
 const searchEl = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
@@ -12,7 +10,7 @@ weatherForm.addEventListener("submit", (e) => {
   messageTwo.textContent = "";
 
   const location = searchEl.value;
-  const url = `http://localhost:3000/weather?address=${location}`;
+  const url = `/weather?address=${location}`;
 
   fetch(url).then((response) => {
     response.json().then((data) => {
